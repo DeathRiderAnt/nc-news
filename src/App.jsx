17c16b00f41395cf92b13af5c7f92bd8a3256c80
DeathRiderAnt <ant_odin@hotmail.co.uk>
2025-11-10 10:50:35 +0000

    Initialised project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello!</h1>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
